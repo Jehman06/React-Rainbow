c06bea1aeaed6c0a38ca9a4f5b438fb8ae9f5bdb
Jeremy Lehmann <jeremylehmann@Jeremys-MacBook-Pro.local>
2023-04-08 11:02:30 -0700

    Created a rainbow

---
+++ src/components/ColorBlock.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function ColorBlock({ color }) {
+    return <div className="colorBlock" style={{ 'backgroundColor': color }}>
+                <p>{color}</p>
+            </div>
+}
